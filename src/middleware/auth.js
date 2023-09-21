0512996d08bf2c9cf80fa591dd29a1e4da542631
valtemir martins <valmartinsfilho@gmail.com>
2023-09-21 20:59:29 -0300

    aperfeiçoando

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -23,6 +23,6 @@ module.exports = (req, res, next) => {
       return res.status(401).send({ error: 'Token mal formatado' });
     }
   } else {
-    next();
+    return res.status(401).send({ error: 'Acesso Negado faça o login' });
   }
 };
